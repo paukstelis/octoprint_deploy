26835f1a653a583fbe6b158499e465589348a58f
paukstelis <paukstel@umd.edu>
2021-03-27 08:52:39 -0400

    modify for ch341

--- addnew_printer.sh
+++ addnew_printer.sh
@@ -127,7 +127,7 @@ then
    counter=0
    while [[ -z "$UDEV" ]] && [[ $counter -lt 30 ]]; do 
       UDEV=$(timeout 1s journalctl -kf | sed -n -e 's/^.*SerialNumber: //p')
-      TEMPUSB=$(timeout 1s journalctl -kf | sed -n -e 's/^.*\(cdc_acm\|ftdi_sio\|ch341\) \([0-9].*[0-9]\): \(tty.*\|FTD.*\|ch341.*\).*/\2/p')   
+      TEMPUSB=$(timeout 1s journalctl -kf | sed -n -e 's/^.*\(cdc_acm\|ftdi_sio\|ch341\) \([0-9].*[0-9]\): \(tty.*\|FTD.*\|ch341-uart.*\).*/\2/p')   
       counter=$(( $counter + 1 ))
    done
 fi
