fdc7a38e3d12e52d9e3640ae31540b9e6025b11d
paukstelis <paukstel@umd.edu>
2022-06-16 20:02:41 -0400

    date check

--- octoprint_deploy.sh
+++ octoprint_deploy.sh
@@ -939,7 +939,8 @@ restart_all() {
 back_up() {
     INSTANCE=$1
     echo "Creating backup of $INSTANCE...."
-    sudo -p $user tar -czf $INSTANCE_$(date '+%Y-%m-%d')_backup.tar.gz /home/$user/.$INSTANCE -C /home/$user
+    d=$(date '+%Y-%m-%d')
+    sudo -p $user tar -czf $INSTANCE_$d_backup.tar.gz /home/$user/.$INSTANCE -C /home/$user
     echo "Tarred and gzipped backup created in /home/$user"
 }
 
